6f1221cf5fe9fb5324cfbc6047466ab90f4edd3d
anton <ageikow@mail.ru>
2024-03-06 18:47:59 +0700

    2 LSTM layers

--- Model.py
+++ Model.py
@@ -9,11 +9,15 @@ def get_model(stackshape):
     model = Sequential()
     model.add(Input(shape=stackshape))
     model.add(ConvLSTM2D(filters=3, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="sigmoid",
+                         recurrent_activation="hard_sigmoid", use_bias=True, kernel_initializer="glorot_uniform",
+                         return_sequences=True))
+    model.add(ConvLSTM2D(filters=3, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="sigmoid",
                          recurrent_activation="hard_sigmoid", use_bias=True, kernel_initializer="glorot_uniform"))
     model.add(BatchNormalization())
 
     model.compile(loss="mse")
     return model
 
+
 if __name__ == "__main__":
-    print(get_model((5,352,640,3)).summary())
+    print(get_model((5, 352, 640, 3)).summary())
